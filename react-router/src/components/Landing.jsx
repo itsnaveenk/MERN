9a7f812fce62324090c87ac4305f36d9c813f426
Naveen Kumar <connect.naveenk@gmail.com>
2024-08-23 01:36:26 +0530

    react-router#1

---
+++ react-router/src/components/Landing.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Welcome to the Landing Page</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default Landing;
