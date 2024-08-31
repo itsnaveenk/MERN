4b9bddbc70df039887276ffaec796e765afd26c1
Naveen Kumar <connect.naveenk@gmail.com>
2024-08-31 15:25:12 +0530

    ""

--- youtube-ui-clone/src/components/AppBar.tsx
+++ youtube-ui-clone/src/components/AppBar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { SearchBar } from './SearchBar';
+import Sidebar from './Sidebar';
 
 const AppBar: React.FC = () => {
   return (
-    <div className="app-bar flex justify-between pt-1">
-      <div className='text-md inline-flex items-center pb-2'>Youtube</div>
-      <div><SearchBar/></div>
-      <div className='text-md inline-flex items-center pb-2'>SignIn</div>
-
+    <div className="grid grid-cols-12">
+      <div className='col-span-1 m-2'><Sidebar /></div>
+      <div className="app-bar flex justify-between pt-1 col-span-11">
+        <div className='text-md inline-flex items-center pb-2'>Youtube</div>
+        <div><SearchBar /></div>
+        <div className='text-md inline-flex items-center pb-2'>SignIn</div>
+      </div>
     </div>
   );
 };
---
+++ youtube-ui-clone/src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="sidebar">
+      {/* Sidebar content goes here */}
+      <span>sidebar</span>
+    </div>
+  );
+};
+
+export default Sidebar;
