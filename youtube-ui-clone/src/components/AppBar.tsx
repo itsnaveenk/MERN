3706ad3ba5632b8a74bbe48ac011dd4c5c5659d3
Naveen Kumar <connect.naveenk@gmail.com>
2024-08-31 17:47:42 +0530

    ""

--- youtube-ui-clone/src/components/AppBar.tsx
+++ youtube-ui-clone/src/components/AppBar.tsx
@@ -4,13 +4,16 @@ import Sidebar from './Sidebar';
 
 const AppBar: React.FC = () => {
   return (
-    <div className="grid grid-cols-12">
-      <div className='col-span-1 m-2'><Sidebar /></div>
-      <div className="app-bar flex justify-between pt-1 col-span-11">
-        <div className='text-md inline-flex items-center pb-2'>Youtube</div>
-        <div><SearchBar /></div>
-        <div className='text-md inline-flex items-center pb-2'>SignIn</div>
-      </div>
+    <div className="flex justify-between	pl-3 pr-3 pt-1">
+        <div>
+            Youtube
+        </div>
+        <div>
+            <SearchBar />
+        </div>
+        <div>
+            SignIn
+        </div>
     </div>
   );
 };
