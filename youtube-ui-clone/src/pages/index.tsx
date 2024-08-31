91fde0318a67a6918a69c4e2c54c2cdd9d043f92
Naveen Kumar <connect.naveenk@gmail.com>
2024-08-31 17:48:16 +0530

    ""

--- youtube-ui-clone/src/pages/index.tsx
+++ youtube-ui-clone/src/pages/index.tsx
@@ -3,16 +3,20 @@ import { Inter } from "next/font/google";
 import VideoGrid from "@/components/VideoGrid";
 import { VideoCard } from "@/components/VideoCard";
 import AppBar from "@/components/AppBar";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      
-      <AppBar></AppBar>
-      <VideoGrid/>
-      
-    </div>
+    <main>
+
+      <AppBar />
+      <div className="flex">
+        <Sidebar />
+        <VideoGrid />
+      </div>
+
+    </main>
   );
 }
